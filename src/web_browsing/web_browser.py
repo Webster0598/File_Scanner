ab925ed060205bfe8f435c3a63c3877e387f83f8
chris <webster0598@gmail.com>
2024-01-29 14:29:56 -0800

    Worked on auto web browser.

--- src/web_browsing/web_browser.py
+++ src/web_browsing/web_browser.py
@@ -2,24 +2,33 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def start():
 
-    driver  = webdriver.Edge()
+    driver = webdriver.Edge()
 
-    driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+    driver.get("https://www.practicefusion.com/")
 
-    title = driver.title
+    wait = WebDriverWait(driver, 10)
 
-    driver.implicitly_wait(0.5)
+    original_window = driver.current_window_handle
 
-    text_box = driver.find_element(by=By.NAME, value="my-text")
-    submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+    assert len(driver.window_handles) == 1
 
-    text_box.send_keys("Selenium")
+    driver.find_element(by=By.CLASS_NAME, value="topnav__login-link").click()
 
-    time.sleep(3)
-    submit_button.click()
+    wait.until(EC.number_of_windows_to_be(2))
+
+    for window_handle in driver.window_handles:
+        if window_handle != original_window:
+            driver.switch_to.window(window_handle)
+            break
+
+    print(driver.current_url)
+    email_input = driver.find_element(by=By.CLASS_NAME, value="loginInput")
+    print(email_input)
 
     time.sleep(3)
     driver.quit()
