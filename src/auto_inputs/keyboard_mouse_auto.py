463ab9ece8cc7f09b053c30717ba84e4acdc6b27
chris <webster0598@gmail.com>
2024-11-13 13:49:51 -0800

    Testing pyautogui.

--- src/auto_inputs/keyboard_mouse_auto.py
+++ src/auto_inputs/keyboard_mouse_auto.py
@@ -10,7 +10,7 @@ def start():
     print(x, y)
     img_loc = pyautogui.locateOnScreen('src/auto_inputs/edge_icon.png', confidence=0.9)
     pyautogui.PAUSE = 2.5
-    print(img_loc[0])
+
     pyautogui.moveTo(img_loc[0], img_loc[1], 1)
     # pyautogui.click(x * 0.6, y * 0.97)
     # pyautogui.click(820, 400)
